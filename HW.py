ab910a796fcd1470af364f005b9af41761e7a807
nikhitha221 <nikhithakowluru@gmail.com>
2025-10-11 15:26:23 +0100

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,4 @@
+day = int(input("Enter the day you were born") )
+month = input("Enter the month you were born")
+year = int(input("Enter the year you were born"))
+print("Your birthday is on" , day , month, year)
